1056c43e9f4bb16d94d1437495186e485adb28df
Jose Ignacio Andres <joserobleda@gmail.com>
2014-01-19 13:26:00 +0100

    express basics

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 
-	var express = require('express');
-	var app 	= express();
+	var path 			= require('path').dirname(require.main.filename);
+	var http 			= require('http');
+	var express 		= require('express');
+	var app 			= express();
+	var cookieSecret 	= 'yoursuperamazincookiesecret';
+	var server 			= http.createServer(app);
+
+
+
+	// default express settings
+	app.use(express.compress());
+	app.use(express.cookieParser());
+	app.use(express.cookieParser(cookieSecret));
+	app.use(express.cookieSession({path: '/', httpOnly: true, maxAge: null, secret: cookieSecret}));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(require('get-methodoverride'));
+	app.use(express.static(path + '/public'));	
+	app.configure('development', function() {
+		app.use(express.errorHandler({dumpExceptions:true, showStack:true}));
+	});
+
+
+	app.start = function () {
+		console.log('App running in port http://localhost:3000');
+		server.listen(3000);
+	};
 
 
 	module.exports = app;
